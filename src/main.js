a7c59d96525340ff1355d17d1f371d04a42e67b7
Mhmod-hsn <hsn.mhmod.96@gmail.com>
2020-04-15 21:58:13 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// import vuesax with icons
+import Vuesax from 'vuesax'
+import 'material-icons/iconfont/material-icons.css';
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+  theme: {
+    colors: {
+      primary: '#f00000',
+    }
+  }
+})
+
+
 Vue.config.productionTip = false;
 
 new Vue({
